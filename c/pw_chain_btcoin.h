399337d311b1779a57df09dbb86875ba9704adff
xcshuan <xcshuan@hotmail.com>
2021-03-21 13:04:59 +0800

    refactor btc and doge sign code

--- c/pw_chain_btcoin.h
+++ c/pw_chain_btcoin.h
@@ -1,123 +1,46 @@
 #include "ckb_syscalls.h"
 #include "common.h"
-// #include "libsig.h"
 #include "pw_k1_helper.h"
-#include "ripemd160.h"
 #include "secp256k1_helper.h"
 #include "sha256.h"
 
 #define u8 unsigned char
-#define BLAKE2B_BLOCK_SIZE 32
-#define RIPEMD160_SIZE 20
-#define SHA256_SIZE 32
-// #define TEMP_LEN 1024
-#define RECID_INDEX 64
-
-#define RECOVERABLE_SIGNATURE_SIZE 65
-#define NONE_RECOVERABLE_SIGNATURE_SIZE 64
-#define COMPRESSED_PUBKEY_SIZE 33
-#define NONE_COMPRESSED_PUBKEY_SIZE 65
-/* RECOVERABLE_SIGNATURE_SIZE + NONE_COMPRESSED_PUBKEY_SIZE */
+#define MESSAGE_HEX_LEN 64
 
 const char BTC_MESSAGE_MAGIC[25] = "Bitcoin Signed Message:\n";
-const char DOGE_MESSAGE_MAGIC[26] = "Dogecoin Signed Message:\n";
-const char HEX_TABLE[] = {'0', '1', '2', '3', '4', '5', '6', '7',
-                          '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+const int8_t BTC_MAGIC_LEN = 24;
 
-void bin_to_hex(unsigned char* source, unsigned char* dest, size_t len) {
-  for (int i = 0; i < len; i++) {
-    dest[i * 2] = HEX_TABLE[source[i] >> 4];
-    dest[i * 2 + 1] = HEX_TABLE[source[i] & 0x0F];
-  }
-  return;
-}
-
-// without pubkey in witness
+/**
+ * @param message transaction message digest for signature verification, size is
+ * 32 bytes
+ * @param btc_address last 20 bytes ripemd160(sha256) hash of pubkey, used to
+ * shield the real pubkey. size is 20 bytes
+ * @param lock_bytes signature signed by BTC wallet, size is 65 bytes.
+ *
+ */
 int validate_btcoin(unsigned char* message, unsigned char* btc_address,
-                    unsigned char* lock_bytes, u8 is_btc) {
-  int ret;
-  unsigned char temp[TEMP_SIZE];
-  uint8_t secp_data[CKB_SECP256K1_DATA_SIZE];
+                    unsigned char* lock_bytes) {
+  u8 temp[MESSAGE_HEX_LEN];
 
   bin_to_hex(message, temp, 32);
 
   SHA256_CTX sha256_ctx;
-  if (is_btc == 0) {
-    u8 MESSAGE_MAGIC[26];
-    MESSAGE_MAGIC[0] = 24;  // MESSAGE_MAGIC length
-    memcpy(&MESSAGE_MAGIC[1], BTC_MESSAGE_MAGIC, 24);
-    MESSAGE_MAGIC[25] = 64;  // message length
 
-    /* Calculate signature message */
-    sha256_init(&sha256_ctx);
-    sha256_update(&sha256_ctx, MESSAGE_MAGIC, 26);
-    sha256_update(&sha256_ctx, temp, 64);
-    sha256_final(&sha256_ctx, message);
+  // len of magic + magic string + len of message, size is 26 Byte
+  u8 MESSAGE_MAGIC[BTC_MAGIC_LEN + 2];
+  MESSAGE_MAGIC[0] = BTC_MAGIC_LEN;  // MESSAGE_MAGIC length
+  memcpy(&MESSAGE_MAGIC[1], BTC_MESSAGE_MAGIC, BTC_MAGIC_LEN);
+  MESSAGE_MAGIC[25] = MESSAGE_HEX_LEN;  // message length
 
-    sha256_init(&sha256_ctx);
-    sha256_update(&sha256_ctx, message, 32);
-    sha256_final(&sha256_ctx, message);
-  } else if (is_btc == 1) {
-    u8 MESSAGE_MAGIC[27];
-    MESSAGE_MAGIC[0] = 25;  // MESSAGE_MAGIC length
-    memcpy(&MESSAGE_MAGIC[1], DOGE_MESSAGE_MAGIC, 25);
-    MESSAGE_MAGIC[26] = 64;  // message length
-
-    /* Calculate signature message */
-    sha256_init(&sha256_ctx);
-    sha256_update(&sha256_ctx, MESSAGE_MAGIC, 27);
-    sha256_update(&sha256_ctx, temp, 64);
-    sha256_final(&sha256_ctx, message);
-
-    sha256_init(&sha256_ctx);
-    sha256_update(&sha256_ctx, message, 32);
-    sha256_final(&sha256_ctx, message);
-  } else {
-    return -102;
-  }
-
-  secp256k1_context context;
-  ret = ckb_secp256k1_custom_verify_only_initialize(&context, secp_data);
-  if (ret != 0) {
-    return ret;
-  }
-
-  int recid = (lock_bytes[0] - 27) & 3;
-  bool fComp = ((lock_bytes[0] - 27) & 4) != 0;
-
-  secp256k1_ecdsa_recoverable_signature signature;
-  if (secp256k1_ecdsa_recoverable_signature_parse_compact(
-          &context, &signature, &lock_bytes[1], recid) == 0) {
-    return ERROR_SECP_PARSE_SIGNATURE;
-  }
-
-  /* Recover pubkey */
-  secp256k1_pubkey pubkey;
-  if (secp256k1_ecdsa_recover(&context, &pubkey, &signature, message) != 1) {
-    return ERROR_SECP_RECOVER_PUBKEY;
-  }
-
-  /* serialize pubkey */
-  size_t pubkey_size =
-      fComp ? COMPRESSED_PUBKEY_SIZE : NONE_COMPRESSED_PUBKEY_SIZE;
-  if (secp256k1_ec_pubkey_serialize(
-          &context, temp, &pubkey_size, &pubkey,
-          fComp ? SECP256K1_EC_COMPRESSED : SECP256K1_EC_UNCOMPRESSED) != 1) {
-    return ERROR_SECP_SERIALIZE_PUBKEY;
-  }
-
-  /* check pubkey hash */
+  /* Calculate signature message */
   sha256_init(&sha256_ctx);
-  sha256_update(&sha256_ctx, temp, pubkey_size);
-  sha256_final(&sha256_ctx, temp);
+  sha256_update(&sha256_ctx, MESSAGE_MAGIC, BTC_MAGIC_LEN + 2);
+  sha256_update(&sha256_ctx, temp, MESSAGE_HEX_LEN);
+  sha256_final(&sha256_ctx, message);
 
-  ripemd160_state ripe160_ctx;
-  ripemd160_init(&ripe160_ctx);
-  ripemd160_update(&ripe160_ctx, temp, SHA256_SIZE);
-  ripemd160_finalize(&ripe160_ctx, temp);
-  if (memcmp(btc_address, temp, RIPEMD160_SIZE) != 0) {
-    return ERROR_PUBKEY_RIPEMD160_HASH;
-  }
+  sha256_init(&sha256_ctx);
+  sha256_update(&sha256_ctx, message, SHA256_SIZE);
+  sha256_final(&sha256_ctx, message);
 
-  return 0;
+  return verify_signature_btc(message, lock_bytes, btc_address);
 }
--- c/pw_k1_helper.h
+++ c/pw_k1_helper.h
@@ -10,11 +10,15 @@
 #include "ckb_syscalls.h"
 #include "keccak256.h"
 #include "protocol.h"
+#include "ripemd160.h"
 #include "secp256k1_helper.h"
+#include "sha256.h"
 
 #define HASH_SIZE 32
 #define BLAKE160_SIZE 20
+#define RIPEMD160_SIZE 20
 #define PUBKEY_SIZE 65  // ETH address uncompress pub key
+#define SHA256_SIZE 32
 #define TEMP_SIZE 32768
 #define RECID_INDEX 64
 /* 32 KB */
@@ -22,6 +26,9 @@
 #define SCRIPT_SIZE 32768
 #define SIGNATURE_SIZE 65
 #define MAX_OUTPUT_LENGTH 64
+#define COMPRESSED_PUBKEY_SIZE 33
+#define NONE_COMPRESSED_PUBKEY_SIZE 65
+
 #define ERROR_TOO_MANY_OUTPUT_CELLS -18
 
 #if (MAX_WITNESS_SIZE > TEMP_SIZE) || (SCRIPT_SIZE > TEMP_SIZE)
@@ -75,6 +82,67 @@ int verify_signature(unsigned char *message, unsigned char *lock_bytes,
 
   if (memcmp(lock_args, &temp[12], BLAKE160_SIZE) != 0) {
     return ERROR_PUBKEY_BLAKE160_HASH;
+  }
+
+  return CKB_SUCCESS;
+}
+
+/**
+ * Verify secp256k1 signature, check the kecack256 hash of pubkey (compressed or
+ * uncompressed) recovered from signature is equal with the lock script arg.
+ *
+ * @param message  the calculated hash of ckb transaciton.
+ * @param lock_bytes the signature from transcation witness.
+ * @param lock_args the args of lock script.
+ *
+ */
+int verify_signature_btc(unsigned char *message, unsigned char *lock_bytes,
+                         const void *lock_args) {
+  unsigned char temp[TEMP_SIZE];
+
+  secp256k1_context context;
+  uint8_t secp_data[CKB_SECP256K1_DATA_SIZE];
+  int ret = ckb_secp256k1_custom_verify_only_initialize(&context, secp_data);
+  if (ret != 0) {
+    return ret;
+  }
+
+  int recid = (lock_bytes[0] - 27) & 3;
+  bool fComp = ((lock_bytes[0] - 27) & 4) != 0;
+
+  secp256k1_ecdsa_recoverable_signature signature;
+  if (secp256k1_ecdsa_recoverable_signature_parse_compact(
+          &context, &signature, &lock_bytes[1], recid) == 0) {
+    return ERROR_SECP_PARSE_SIGNATURE;
+  }
+
+  /* Recover pubkey */
+  secp256k1_pubkey pubkey;
+  if (secp256k1_ecdsa_recover(&context, &pubkey, &signature, message) != 1) {
+    return ERROR_SECP_RECOVER_PUBKEY;
+  }
+
+  /* serialize pubkey */
+  size_t pubkey_size =
+      fComp ? COMPRESSED_PUBKEY_SIZE : NONE_COMPRESSED_PUBKEY_SIZE;
+  if (secp256k1_ec_pubkey_serialize(
+          &context, temp, &pubkey_size, &pubkey,
+          fComp ? SECP256K1_EC_COMPRESSED : SECP256K1_EC_UNCOMPRESSED) != 1) {
+    return ERROR_SECP_SERIALIZE_PUBKEY;
+  }
+
+  SHA256_CTX sha256_ctx;
+  /* check pubkey hash */
+  sha256_init(&sha256_ctx);
+  sha256_update(&sha256_ctx, temp, pubkey_size);
+  sha256_final(&sha256_ctx, temp);
+
+  ripemd160_state ripe160_ctx;
+  ripemd160_init(&ripe160_ctx);
+  ripemd160_update(&ripe160_ctx, temp, SHA256_SIZE);
+  ripemd160_finalize(&ripe160_ctx, temp);
+  if (memcmp(lock_args, temp, RIPEMD160_SIZE) != 0) {
+    return ERROR_PUBKEY_RIPEMD160_HASH;
   }
 
   return CKB_SUCCESS;
@@ -189,4 +257,16 @@ int get_signature_from_trancation(uint64_t *chain_id, unsigned char *message,
   keccak_final(&sha3_ctx, message);
   return CKB_SUCCESS;
 }
+
+const char HEX_TABLE[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                          '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+void bin_to_hex(unsigned char *source, unsigned char *dest, size_t len) {
+  for (int i = 0; i < len; i++) {
+    dest[i * 2] = HEX_TABLE[source[i] >> 4];
+    dest[i * 2 + 1] = HEX_TABLE[source[i] & 0x0F];
+  }
+  return;
+}
+
 #endif
