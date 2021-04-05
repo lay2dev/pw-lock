f33168fcf20ff0524237c57e5b353610f3a224f3
xjd <xjd@cryptape.com>
2021-04-05 15:31:23 +0800

    add fuzzer test

---
+++ tests/pw_webauthn_lib/README.md
@@ -0,0 +1,11 @@
+### Fuzzing test
+```bash
+make
+make start-fuzzer
+```
+
+Get code coverage by:
+```bash
+make report
+make show
+```
---
+++ tests/pw_webauthn_lib/main_test.c
@@ -0,0 +1,13 @@
+
+#include "ckb_syscalls.h"
+#include "webauthn/pw_webauthn_lib.c"
+#include "stubs.h"
+
+
+int verify_challenge_in_client_data(const u8* digest_message,
+                                    const u8* client_data,
+                                    int client_data_len);
+int main() {
+
+}
+
